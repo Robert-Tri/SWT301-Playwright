1029d6defe82c50bccff29acbc5f3339a9b93bb1
Robert-Tri <ductri3122002@gmail.com>
2023-07-05 20:30:53 +0700

    add testLogin use DDT

--- src/test/java/com/dentcare/test/TestLogin.java
+++ src/test/java/com/dentcare/test/TestLogin.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import com.microsoft.playwright.*;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 /**
  *
@@ -30,17 +32,54 @@ public class TestLogin {
 
     @AfterAll
     public static void tearDown() {
-        page.waitForTimeout(7000); // Dừng thực thi trong 7 giây
+        page.waitForTimeout(3000); // Dừng thực thi trong 3 giây
         browser.close();
         playwright.close();
     }
 
     @Test
     public void testPageTitle() {
-        page.navigate("http://localhost:8080/SWP391-SE1743");
+        page.navigate("http://localhost:8080/SWP391-SE1743/login.jsp");
         String pageTitle = page.title();
         Assertions.assertEquals("DentCare", pageTitle);
-        
+        // Ghi lại các hành động đăng nhập
+        page.fill("#user", "usertestdcbs@gmail.com");
+        page.fill("#pass", "User123456");
+        page.click("//button[contains(text(),'Đăng Nhập')]");
+        String currentUrl = page.url();  
+        Assertions.assertEquals("http://localhost:8080/SWP391-SE1743/MainController", currentUrl);    
+        page.hover("//a[@class='dropdown-toggle nav-link user-link']");
+        page.waitForSelector("//button[contains(text(),'Tài Khoản')]");
+        page.click("//button[contains(text(),'Tài Khoản')]");
+        String currentContent = page.textContent("li:nth-child(2) span:nth-child(2) a:nth-child(1)"); 
+        Assertions.assertEquals("usertestdcbs@gmail.com", currentContent);
+    }
+    
+    //DDT 
+    public static Object[][] initData(){    
+        return new Object[][]{
+                                {"ductri3122002@gmail.com", "User123456"},
+                                {"levannhat574602@gmail.com", "User123456"},
+                                {"phansongthao203@gmail.com", "User123456"}
+                             };
+    }
+    @ParameterizedTest
+    @MethodSource(value = "initData")
+    public void testLogin_DDT(String email, String password) throws InterruptedException{
+        page.navigate("http://localhost:8080/SWP391-SE1743/login.jsp");
+        String pageTitle = page.title();
+        Assertions.assertEquals("DentCare", pageTitle);
+        // Ghi lại các hành động đăng nhập
+        page.fill("#user", email);
+        page.fill("#pass", password);
+        page.click("//button[contains(text(),'Đăng Nhập')]");
+        page.waitForTimeout(3000);
+        page.hover("//a[@class='dropdown-toggle nav-link user-link']");
+        page.waitForSelector("//button[contains(text(),'Tài Khoản')]");
+        page.click("//button[contains(text(),'Tài Khoản')]");
+        page.waitForTimeout(3000);
+        String currentContent = page.textContent("li:nth-child(2) span:nth-child(2) a:nth-child(1)"); 
+        Assertions.assertEquals(email, currentContent);
     }
     
 }
